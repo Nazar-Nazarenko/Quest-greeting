390be2fe6a6ba297539b3f2c765287f381cfb00d
nazarnazarenko <nanazarenko@deloitte.com>
2025-10-04 18:08:01 +0300

    global layout added

--- quest-greeting/src/app/home/home.ts
+++ quest-greeting/src/app/home/home.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {GlobalLayout} from '../infrastructure/global-layout/global-layout';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    GlobalLayout
+  ],
   templateUrl: './home.html',
   standalone: true,
   styleUrl: './home.scss'
---
+++ quest-greeting/src/app/infrastructure/global-layout/global-layout.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-global-layout',
+  imports: [],
+  standalone: true,
+  templateUrl: './global-layout.html',
+  styleUrl: './global-layout.scss'
+})
+export class GlobalLayout {
+
+}
